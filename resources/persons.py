ff1ccbc45a5481634f28959e1af989d13b2ce6bd
Bryan Geil <beg692@gmail.com>
2019-11-14 14:20:40 -0600

    added create and show route

--- resources/persons.py
+++ resources/persons.py
@@ -8,7 +8,8 @@ from playhouse.shortcuts import model_to_dict
 
 person = Blueprint('persons', 'person')
 
-@person.route('/', methods=['GET'])
+# index route
+@person.route('/', methods=["GET"])
 def get_all_persons():
 	try:
 		persons = [model_to_dict(person) for person in models.Person.select()]
@@ -16,3 +17,22 @@ def get_all_persons():
 		return jsonify(data=persons, status={"code": 200, "message": "Success"}), 200
 	except models.DoesNotExist:
 		return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"}), 401
+
+# create route
+@person.route('/', methods=["POST"])
+def create_person():
+	payload = request.get_json()
+	print(type(payload), 'payload')
+
+	person = models.Person.create(**payload)
+	print(dir(person))
+	print(model_to_dict(person), 'model_to_dict')
+	person_dict = model_to_dict(person)
+	return jsonify(data=person_dict, status={"code":201, "message": "Success"})
+
+# show route
+@person.route('/<id>', methods=["GET"])
+def show_person(id):
+	print(id)
+	person = models.Person.get_by_id(id)
+	return jsonify(data=model_to_dict(person), status={"code":201, "message": "Success"})
